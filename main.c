ac10da98bda5afd3393a3cbeaef5be70ac994a31
Abhishek Ojha <abhishek.ojha@vvdntech.in>
2021-11-14 10:52:55 +0530

    Add dummy file.Will be deleted later

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+
+int create_child(){
+	pid_t child_pid = fork();
+	int a=0;
+		switch(child_pid) {
+		case -1:
+			printf("Error Case");
+			break;
+
+		case 0:
+			printf("Child Process\n");
+			printf("Calling EXECVP\n");
+
+			char* argument_list[] = {"ls", "-l", NULL}; // NULL terminated array of char* strings
+			// Ok! Will execute the command "ls -l"
+			auto ret=execvp("ls", argument_list);
+			printf("End Calling EXECVP\n");
+
+		default:
+			printf("parent process\n");
+			a=1;
+			break;
+		}
+
+	return a;
+}
+
+int main(){
+printf("Value of function=%d", create_child());
+if(1)
+	printf("TRUE");
+else
+	printf("False");
+
+
+return 0;
+}
